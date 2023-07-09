395b8eea8fda65fc306c09fedecb25f5c72116e8
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-07-09 14:45:11 +0200

    fix table write

--- tree_data/utils/interact_with_database.py
+++ tree_data/utils/interact_with_database.py
@@ -28,32 +28,5 @@ def get_db_engine():
 def add_to_db(engine, result, table_name):
     result['geometry'] = gpd.points_from_xy(result.lat, result.lng)
     result = result.rename(columns={'geometry': 'geom'}).set_geometry('geom')
-    result.to_postgis('added_trees_tmp', engine, if_exists='replace', index=False)
-    try:
-        # execute sql query for adding the data
-        sql = "UPDATE added_trees_tmp SET geom = ST_SetSRID(geom,4326)"
-
-        with engine.connect() as conn:
-            # there is a problem with uppercase header names, so we have to bring all column names to "" here
-            conn.execute(sql)
-
-        with engine.connect() as conn:
-            cols = ''
-            for c in result.columns:
-                cols += '"%s", ' % c
-            cols = cols[:-2]
-
-            append_sql = f"INSERT INTO {table_name}({cols}) SELECT * FROM added_trees_tmp"
-            conn.execute(append_sql)
-
-            logger.info(
-                "Successfully added " + str(len(result)) + " new trees to the database table '" + table_name + "'.")
-
-    except Exception as e:
-        logger.info('No trees to add.')
-        logging.exception('Error occurred while adding trees to database: {}'.format(e))
-
-    with engine.connect() as conn:
-        # delete the temporary table
-        sql = 'DROP TABLE added_trees_tmp'
-        conn.execute(sql)
+    result.to_postgis(table_name, engine, if_exists='replace', index=False)
+    logger.info(f'Imported trees to table {table_name}')
