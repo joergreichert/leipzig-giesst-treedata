564cb5cfcc9bf4e71f920978dc951488c9d5bd06
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2023-07-21 23:11:36 +0200

    update radolan_geometry from radolan_temp

--- treedata/radolan/upload_radolan.py
+++ treedata/radolan/upload_radolan.py
@@ -1,17 +1,28 @@
 from sqlalchemy import text
 import logging
-from shapely.wkt import dumps
+from shapely.wkt import loads, dumps
 import pandas
 
 logger = logging.getLogger(__name__)
 
 
+def update_radolan_geometry(conn):
+    count = conn.execute(text('SELECT count(*) FROM public.radolan_geometry')).scalar()
+    if count == 0:
+        conn.execute(text('''
+            INSERT INTO radolan_geometry (geometry)
+            SELECT distinct(geometry) FROM radolan_temp     
+        '''))
+        conn.execute(text('UPDATE radolan_geometry SET centroid = ST_Centroid (geometry)'))
+
+
 def upload_radolan_data(engine, radolan_data):
     radolan_data = radolan_data.rename(columns={'MYFLD': 'value'})
     radolan_data['measured_at'] = pandas.to_datetime(radolan_data['measured_at'])
-    # radolan_data['geometry'] = dumps(radolan_data['geometry'], rounding_precision=5)
+    radolan_data['geometry'] = loads(dumps(radolan_data['geometry'], rounding_precision=5))
     radolan_data.to_postgis('radolan_temp', engine, if_exists='replace', index=False)
     with engine.connect() as conn:
+        update_radolan_geometry(conn)
         conn.execute(text('''
             INSERT INTO "public".radolan_data(geom_id, value, measured_at) 
             SELECT radolan_geometry.id, radolan_temp.value, radolan_temp.measured_at 
